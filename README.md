fe50b8d3be0e341d4fcf6309631f8ca3e7976822
Ngo Jun Hao Jason <njhjason@protonmail.com>
2024-08-14 11:35:22 +1200

    Set up project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Kakapo Lodge Backend
+
+Backend service for Kakapo Lodge website  
+Runs on Digital Ocean serverless function
+
+Copyright (c) 2024, James Bae
---
+++ main.js
@@ -0,0 +1,10 @@
+async function main(args) {
+  const BASE_URL =
+    "https://apac.littlehotelier.com/api/v1/properties/kakapolodgedirect/rates.json";
+  const startDate = args["start_Date"] || "";
+  const endDate = args["end_date"] || "";
+
+  const url = `${BASE_URL}?start_date=${startDate}&end_date=${endDate}`;
+  const response = await fetch(url);
+  return { body: await response.json() };
+}
