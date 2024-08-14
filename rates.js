120422c6de6720928dbb4162e2513f895ffe4716
Ngo Jun Hao Jason <njhjason@protonmail.com>
2024-08-14 14:34:15 +1200

    Fix typo

--- rates.js
+++ rates.js
@@ -1,7 +1,7 @@
 async function main(args) {
   const BASE_URL =
     "https://apac.littlehotelier.com/api/v1/properties/kakapolodgedirect/rates.json";
-  const startDate = args["start_Date"] || "";
+  const startDate = args["start_date"] || "";
   const endDate = args["end_date"] || "";
 
   const url = `${BASE_URL}?start_date=${startDate}&end_date=${endDate}`;
