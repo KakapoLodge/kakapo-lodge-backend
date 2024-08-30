ea51ba1abe0567320bf1a28a2d79a9aba1a4d814
Ngo Jun Hao Jason <njhjason@protonmail.com>
2024-08-30 14:48:47 +1200

    Add contact form email function

--- rates.js
+++ rates.js
@@ -1,10 +1,10 @@
 async function main(args) {
-  const BASE_URL =
+  const baseURL =
     "https://apac.littlehotelier.com/api/v1/properties/kakapolodgedirect/rates.json";
   const startDate = args["start_date"] || "";
   const endDate = args["end_date"] || "";
 
-  const url = `${BASE_URL}?start_date=${startDate}&end_date=${endDate}`;
+  const url = `${baseURL}?start_date=${startDate}&end_date=${endDate}`;
   const response = await fetch(url);
   return { body: await response.json() };
 }
